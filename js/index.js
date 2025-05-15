022481f2bc909365ee4fe2c003766f465c8737ab
Ale Suarez <alemsuarez@gmail.com>
2025-05-15 08:49:46 -0300

    correccion desafio completo

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-chars1 = {
+const chars1 = {
     "Mario": "Mario",
     "Luigi": "Luigi",
     "Bowser": "Bowser Morton Koopa",
@@ -8,6 +8,7 @@ chars1 = {
     "Toadette": "Toadette",
     "Daisy": "Princesa Daisy"
 }
+
 const readChars = async () => {
     const response = await fetch("data/chars.json", { method: "GET" })
     // await espera a que fetch devuelva datos para pasar de renglon
@@ -16,32 +17,114 @@ const readChars = async () => {
    // console.log(chars)
     return chars
 }
+let charActual = -1
+// PUNTO 1
 
+//selectedChar = prompt("¿Quién se presenta hoy? \n (Mario, Luigi, Bowser, Peach, Yoshi, Toad, Toadette, Daisy)")
+//console.log("Se eligió : ", selectedChar)
+// PUNTO 6 EL PROMPT VA AL BOTON
 
-selectedChar = prompt("¿Quién se presenta hoy? \n (Mario, Luigi, Bowser, Peach, Yoshi, Toad, Toadette, Daisy)")
-console.log("Se eligió : ", selectedChar)
+//console.log(spanTag.innerText)
 
-const spanTag = document.getElementsByTagName("span")[0] // trae un array
-console.log(spanTag.innerText)
+//spanTag.innerText = selectedChar
 
-spanTag.innerText = selectedChar
-
-console.log(spanTag)
+//console.log(spanTag)
 //console.log(chars[spanTag])
-
-readChars()
+const spanTag = document.getElementsByTagName("span")[0] // trae un arrayPeachdddd
+const leerChars = () => {
+    readChars()
     .then( data => {
         
-       // console.log(data)
-        findChar(data)
+            // console.log(data)
+            // PUNTO 2 MOSTRAR EL NOMBRE EN EL SPAN 
+            nameExt = findChar(data)
+            if (nameExt > -1)
+            {
+                spanTag.innerText = nameExt
+                //showChar(selectedChar)
+                
+            }
+            else
+                spanTag.innerText = "(Desconocido)"     
+            
         } 
     )
-    .catch( err => console.log(err) )
+    .catch( 
+        err => console.log(err)
+        //spanTag.innerText = "(Desconocido)" 
+    )
+}
+
+const addEvents = () => {
+    readChars()
+    .then( data => {
+            constAddEvent(data)
+        } 
+    )
+    .catch( 
+        err => console.log(err)
+        //spanTag.innerText = "(Desconocido)" 
+    )
+}
 
 
 const findChar = array => {
     console.log(array)
     const indice = array.findIndex(personaje => personaje.name === selectedChar)  // busca coincidencia
-    console.log("findIndex", indice)
+    if (indice == -1)
+        return -1 // no existe el nombre
+
+    indexActual = indice // para el cambio punto bonus
+    console.log("findIndex ", indice)
     console.log(array[indice].extName)
+    showChar(selectedChar)
+    return array[indice].extName
 }
+
+const showChar = (selectedChar) => {
+    //alert("por acaLuig")
+    let idSelected = selectedChar.toLowerCase()
+    console.log("id elemento seleccionado", idSelected)
+    if (charActual == idSelected) // si se clickea el que está presionado
+    {
+        document.getElementById(charActual).setAttribute('title', "");   
+        charActual = -1
+        return 0    
+    }
+    //document.getElementById(idSelected).title = "Presentado"
+    if (charActual != -1)
+        document.getElementById(charActual).setAttribute('title', "");   
+    document.getElementById(idSelected).setAttribute('title', "Presentado");
+    
+    charActual = idSelected // para el cambio punto bonus
+   
+
+}
+
+const showPrompt = () => {
+    selectedChar = prompt("¿Quién se presenta hoy? \n (Mario, Luigi, Bowser, Peach, Yoshi, Toad, Toadette, Daisy)")
+    console.log("Se eligió : ", selectedChar)
+    let boton = document.getElementById("showButton")
+    // boton.style.display = "none" punto 6 pide que desaparezca el boton
+}
+
+let boton = document.getElementById("showButton")
+boton.addEventListener("click", function (e) {
+    showPrompt();
+    leerChars();
+});
+
+
+const constAddEvent = (array) => {
+    //alert("entra")
+    for (char of array) 
+    {
+        // recorro los personajes y por cada uno le agrego el evento click
+        console.log()
+        let divChar = document.getElementById(char.name.toLowerCase())
+        divChar.addEventListener("click", function (e) {
+            showChar(divChar.id)
+        })
+    }
+}
+addEvents() // leo el json y dentro llamo a la funcion constAddEvent
